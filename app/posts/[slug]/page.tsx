6a45213818548eb7d7811b283cca1694d41e1fa1
Omotosho Sunday <omotoshosunday360@gmail.com>
2023-06-28 05:35:09 -0800

    Update page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -5,7 +5,7 @@ import getPostMetadata from "../../../components/getPostMetadata";
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
-  const file = `${folder}${slug}.md`;
+  const file = `${folder}${slug}.mdx`;
   const content = fs.readFileSync(file, "utf8");
   const matterResult = matter(content);
   return matterResult;
